5d4ce2a5211c267a26ce74026abbfdf1ab58d29b
ColabLearn <tunji4physics@gmail.com>
2022-12-22 23:11:38 +0000

    Classes and Objects -- Add method to count cars in stock, add car to stock, and display cars upto the maximum display.

--- src/CarDealership.java
+++ src/CarDealership.java
@@ -43,4 +43,31 @@ public class CarDealership {
     public void setCarInStock(Car[] carInStock) {
         this.carInStock = carInStock;
     }
+    public  int countCarsInStock (){
+        return this.carInStock.length;
+    }
+
+    public void addCarToStock(Car car) {
+        int oldLength = this.carInStock.length;
+        int newLength = oldLength+1;
+        Car [] newStock = new Car[newLength];
+
+        for (int i=0; i<=newLength-2; i++){
+            newStock[i]=this.carInStock[i];
+        }
+        newStock[newLength-1] =car;
+
+        this.setCarInStock(newStock);
+    }
+    public void displayStock(){
+        if (this.carInStock.length<=this.maxCarDisplay) {
+            for (Car car: carInStock) {
+                System.out.println(car);
+            }
+        }else {
+            for (int i=0; i<this.maxCarDisplay-1; i++){
+                System.out.println(carInStock);
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,38 @@
 public class Main {
     public static void main(String[] args) {
 
-        Car obaCar = new Car(
+        Car Car1 = new Car(
                 "Toyota",
                 Car.InputPrice(1300.50),
                 CarEngineType.Petrol
         );
 
-        Car semiCar = new Car();
-        semiCar.setManufacturer("Vauxhall");
-        semiCar.setPrice(Car.InputPrice(1300.40));
-        semiCar.setEngineType(CarEngineType.Diesel);
+        Car Car2 = new Car();
+        Car2.setManufacturer("Vauxhall");
+        Car2.setPrice(Car.InputPrice(1300.40));
+        Car2.setEngineType(CarEngineType.Diesel);
 
-        Car timiCar = new Car();
-        timiCar.setManufacturer("Mercedes");
-        timiCar.setEngineType(CarEngineType.Hybrid);
+        Car Car3 = new Car();
+        Car3.setManufacturer("Mercedes");
+        Car3.setEngineType(CarEngineType.Hybrid);
 
-        System.out.println(obaCar);
-        System.out.println(semiCar);
-        System.out.println(timiCar);
-        System.out.println(semiCar.getPrice().getClass() );
+//        System.out.println(Car1);
+//        System.out.println(Car2);
+//        System.out.println(Car3);
+//        System.out.println(Car2.getPrice().getClass() );
+
+        Car [] KaratuStock = {Car1,Car2};
+        CarDealership dealer1 = new CarDealership(
+                "Karatu Cars",
+                5,
+                KaratuStock
+        );
+
+        System.out.println(dealer1.countCarsInStock());
+        dealer1.displayStock();
+        dealer1.addCarToStock(Car3);
+        System.out.println(dealer1.countCarsInStock());
+        dealer1.displayStock();
 
     }
 }
